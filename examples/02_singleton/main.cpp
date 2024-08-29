cbef8fd13ba0d6219c0d8146ab2b1e7f2008ca83
Ruben Sørensen <rsorensen@tutanota.com>
2024-08-29 17:10:44 +0200

    Updated singleton API according to recent main API updates. Created singleton example.

---
+++ examples/02_singleton/main.cpp
@@ -0,0 +1,38 @@
+#include <CLArgs/flag.hpp>
+#include <CLArgs/option.hpp>
+#include <CLArgs/parser.hpp>
+#include <CLArgs/singleton.hpp>
+
+#include <filesystem>
+#include <sstream>
+#include <string_view>
+
+using VerboseFlag   = CLArgs::Flag<"--verbose,-v", "Enable verbose output", false>;
+using ConfigOption  = CLArgs::Option<"--configuration,--config,-c", "FILEPATH", "Specify path to configuration file", true, std::filesystem::path>;
+
+using Parser = CLArgs::ParserSingleton<VerboseFlag, ConfigOption>;
+
+int
+main(int argc, char **argv)
+{
+    try
+    {
+        Parser::parse(argc, argv);
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Error: " << e.what() << '\n';
+        std::cerr << Parser::help() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const bool has_verbose = Parser::has_flag<VerboseFlag>();
+    std::cout << "Has verbose option: " << std::boolalpha << has_verbose << "\n";
+
+    if (const auto file = Parser::get_option<ConfigOption>(); file.has_value())
+    {
+        std::cout << "Config file: " << file.value() << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
