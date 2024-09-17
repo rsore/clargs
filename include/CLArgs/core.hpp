c68d7889e120b0b684b014aac01ba8b0defce1f1
Ruben Sørensen <73177332+rsore@users.noreply.github.com>
2024-09-17 23:30:41 +0200

    Moved identifier_length and max_identifier_length to core.hpp and renamed to indentifier_list_length and max_identifier_list_length

--- include/CLArgs/core.hpp
+++ include/CLArgs/core.hpp
@@ -7,6 +7,7 @@
 #include <algorithm>
 #include <array>
 #include <concepts>
+#include <cstdint>
 #include <iostream>
 #include <string_view>
 
@@ -17,6 +18,41 @@ namespace CLArgs
 
     template <typename T>
     concept Parsable = CmdFlag<T> || CmdOption<T>;
+
+    template <Parsable Parsable>
+    static consteval std::size_t identifier_list_length();
+
+    template <Parsable This, Parsable... Rest>
+    static consteval std::size_t max_identifier_list_length();
 } // namespace CLArgs
+
+template <CLArgs::Parsable Parsable>
+consteval std::size_t
+CLArgs::identifier_list_length()
+{
+    std::size_t length{};
+
+    for (const auto identifier : Parsable::identifiers)
+    {
+        length += identifier.length();
+    }
+    length += (Parsable::identifiers.size() - 1) * 2; // Account for ", " between identifiers
+
+    return length;
+}
+
+template <CLArgs::Parsable This, CLArgs::Parsable... Rest>
+consteval std::size_t
+CLArgs::max_identifier_list_length()
+{
+    std::size_t max_length = identifier_list_length<This>();
+
+    if constexpr (sizeof...(Rest) > 0)
+    {
+        max_length = std::max(max_length, max_identifier_list_length<Rest...>());
+    }
+
+    return max_length;
+}
 
 #endif
