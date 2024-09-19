a847df2607f34a183b2ec8406090395e07857418
Ruben Sørensen <73177332+rsore@users.noreply.github.com>
2024-09-19 22:25:39 +0200

    parse_value<T>() handles chars better

--- tests/parse_value_test.cpp
+++ tests/parse_value_test.cpp
@@ -11,6 +11,7 @@ TEMPLATE_TEST_CASE("parse_value() can parse all integer types",
                    int,
                    long,
                    long long,
+                   std::int8_t,
                    std::int16_t,
                    std::int32_t,
                    std::int64_t,
@@ -18,12 +19,13 @@ TEMPLATE_TEST_CASE("parse_value() can parse all integer types",
                    unsigned int,
                    unsigned long,
                    unsigned long long,
+                   std::uint8_t,
                    std::uint16_t,
                    std::uint32_t,
                    std::uint64_t,
                    std::size_t)
 {
-    CHECK_THROWS(CLArgs::parse_value<TestType>(""));
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>(""), CLArgs::ParseValueException<TestType>);
 
     CHECK(std::is_same_v<decltype(CLArgs::parse_value<TestType>("0")), TestType>);
 
@@ -38,7 +40,7 @@ TEMPLATE_TEST_CASE("parse_value() can parse all integer types",
     }
     else
     {
-        CHECK_THROWS(CLArgs::parse_value<TestType>("-100"));
+        CHECK_THROWS_AS(CLArgs::parse_value<TestType>("-100"), CLArgs::ParseValueException<TestType>);
     }
 }
 
@@ -46,10 +48,12 @@ TEMPLATE_TEST_CASE("parse_value() performs integer bounds checking",
                    "[parse_value]",
                    short,
                    int,
+                   std::int8_t,
                    std::int16_t,
                    std::int32_t,
                    unsigned short,
                    unsigned int,
+                   std::uint8_t,
                    std::uint16_t,
                    std::uint32_t)
 {
@@ -58,9 +62,11 @@ TEMPLATE_TEST_CASE("parse_value() performs integer bounds checking",
 
     if constexpr (std::is_signed_v<TestType>)
     {
-        CHECK_THROWS(CLArgs::parse_value<TestType>(std::to_string(static_cast<std::int64_t>(std::numeric_limits<TestType>::min()) - 1)));
+        CHECK_THROWS_AS(CLArgs::parse_value<TestType>(std::to_string(static_cast<std::int64_t>(std::numeric_limits<TestType>::min()) - 1)),
+                        CLArgs::ParseValueException<TestType>);
     }
-    CHECK_THROWS(CLArgs::parse_value<TestType>(std::to_string(static_cast<std::uint64_t>(std::numeric_limits<TestType>::max()) + 1)));
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>(std::to_string(static_cast<std::uint64_t>(std::numeric_limits<TestType>::max()) + 1)),
+                    CLArgs::ParseValueException<TestType>);
 }
 
 TEMPLATE_TEST_CASE("parse_value() verifies formatting",
@@ -69,6 +75,7 @@ TEMPLATE_TEST_CASE("parse_value() verifies formatting",
                    int,
                    long,
                    long long,
+                   std::int8_t,
                    std::int16_t,
                    std::int32_t,
                    std::int64_t,
@@ -76,19 +83,20 @@ TEMPLATE_TEST_CASE("parse_value() verifies formatting",
                    unsigned int,
                    unsigned long,
                    unsigned long long,
+                   std::uint8_t,
                    std::uint16_t,
                    std::uint32_t,
                    std::uint64_t,
                    std::size_t)
 {
-    CHECK_THROWS(CLArgs::parse_value<TestType>(""));
-    CHECK_THROWS(CLArgs::parse_value<TestType>("   "));
-    CHECK_THROWS(CLArgs::parse_value<TestType>(" 123"));
-    CHECK_THROWS(CLArgs::parse_value<TestType>("123 "));
-    CHECK_THROWS(CLArgs::parse_value<TestType>("abc"));
-    CHECK_THROWS(CLArgs::parse_value<TestType>("12abc34"));
-    CHECK_THROWS(CLArgs::parse_value<TestType>("12.34"));
-    CHECK_THROWS(CLArgs::parse_value<TestType>("@!#$"));
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>(""), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("   "), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>(" 123"), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("123 "), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("abc"), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("12abc34"), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("12.34"), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("@!#$"), CLArgs::ParseValueException<TestType>);
 }
 
 TEMPLATE_TEST_CASE("parse_value() can parse hexadecimal values for unsigned types",
@@ -97,6 +105,7 @@ TEMPLATE_TEST_CASE("parse_value() can parse hexadecimal values for unsigned type
                    int,
                    long,
                    long long,
+                   std::int8_t,
                    std::int16_t,
                    std::int32_t,
                    std::int64_t,
@@ -104,6 +113,7 @@ TEMPLATE_TEST_CASE("parse_value() can parse hexadecimal values for unsigned type
                    unsigned int,
                    unsigned long,
                    unsigned long long,
+                   std::uint8_t,
                    std::uint16_t,
                    std::uint32_t,
                    std::uint64_t,
@@ -117,11 +127,11 @@ TEMPLATE_TEST_CASE("parse_value() can parse hexadecimal values for unsigned type
     }
     else
     {
-        CHECK_THROWS(CLArgs::parse_value<TestType>("0xFF"));
-        CHECK_THROWS(CLArgs::parse_value<TestType>("0XFF"));
+        CHECK_THROWS_AS(CLArgs::parse_value<TestType>("0xFF"), CLArgs::ParseValueException<TestType>);
+        CHECK_THROWS_AS(CLArgs::parse_value<TestType>("0XFF"), CLArgs::ParseValueException<TestType>);
     }
-    CHECK_THROWS(CLArgs::parse_value<TestType>("0x"));
-    CHECK_THROWS(CLArgs::parse_value<TestType>("0X"));
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("0x"), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("0X"), CLArgs::ParseValueException<TestType>);
 }
 
 TEMPLATE_TEST_CASE("parse_value() can parse binary values for unsigned types",
@@ -130,6 +140,7 @@ TEMPLATE_TEST_CASE("parse_value() can parse binary values for unsigned types",
                    int,
                    long,
                    long long,
+                   std::int8_t,
                    std::int16_t,
                    std::int32_t,
                    std::int64_t,
@@ -137,6 +148,7 @@ TEMPLATE_TEST_CASE("parse_value() can parse binary values for unsigned types",
                    unsigned int,
                    unsigned long,
                    unsigned long long,
+                   std::uint8_t,
                    std::uint16_t,
                    std::uint32_t,
                    std::uint64_t,
@@ -149,16 +161,16 @@ TEMPLATE_TEST_CASE("parse_value() can parse binary values for unsigned types",
     }
     else
     {
-        CHECK_THROWS(CLArgs::parse_value<TestType>("0b100"));
-        CHECK_THROWS(CLArgs::parse_value<TestType>("0B100"));
+        CHECK_THROWS_AS(CLArgs::parse_value<TestType>("0b100"), CLArgs::ParseValueException<TestType>);
+        CHECK_THROWS_AS(CLArgs::parse_value<TestType>("0B100"), CLArgs::ParseValueException<TestType>);
     }
-    CHECK_THROWS(CLArgs::parse_value<TestType>("0b"));
-    CHECK_THROWS(CLArgs::parse_value<TestType>("0B"));
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("0b"), CLArgs::ParseValueException<TestType>);
+    CHECK_THROWS_AS(CLArgs::parse_value<TestType>("0B"), CLArgs::ParseValueException<TestType>);
 }
 
 TEST_CASE("parse_value() can parse boolean values", "[parse_value]")
 {
-    REQUIRE_THROWS(CLArgs::parse_value<bool>(""));
+    CHECK_THROWS_AS(CLArgs::parse_value<bool>(""), CLArgs::ParseValueException<bool>);
 
     CHECK(CLArgs::parse_value<bool>("true") == true);
     CHECK(CLArgs::parse_value<bool>("True") == true);
@@ -184,5 +196,26 @@ TEST_CASE("parse_value() can parse boolean values", "[parse_value]")
     CHECK(CLArgs::parse_value<bool>("N") == false);
     CHECK(CLArgs::parse_value<bool>("0") == false);
 
-    REQUIRE_THROWS(CLArgs::parse_value<bool>("enable"));
+    CHECK_THROWS_AS(CLArgs::parse_value<bool>("enable"), CLArgs::ParseValueException<bool>);
+}
+
+TEST_CASE("parse_value() can parse characters", "[parse_value]")
+{
+    CHECK_THROWS_AS(CLArgs::parse_value<char>(""), CLArgs::ParseValueException<char>);
+
+    CHECK(CLArgs::parse_value<char>("a") == 'a');
+    CHECK(CLArgs::parse_value<char>("Z") == 'Z');
+    CHECK(CLArgs::parse_value<char>("1") == '1');
+    CHECK(CLArgs::parse_value<char>("@") == '@');
+    CHECK(CLArgs::parse_value<char>(" ") == ' ');
+    CHECK(CLArgs::parse_value<char>("\n") == '\n');
+    CHECK(CLArgs::parse_value<char>("\t") == '\t');
+
+    CHECK_THROWS_AS(CLArgs::parse_value<char>("ab"), CLArgs::ParseValueException<char>);
+    CHECK_THROWS_AS(CLArgs::parse_value<char>("123"), CLArgs::ParseValueException<char>);
+    CHECK_THROWS_AS(CLArgs::parse_value<char>("abc"), CLArgs::ParseValueException<char>);
+    CHECK_THROWS_AS(CLArgs::parse_value<char>("  "), CLArgs::ParseValueException<char>);
+
+    CHECK_THROWS_AS(CLArgs::parse_value<char>("\0"), CLArgs::ParseValueException<char>);
+    CHECK_THROWS_AS(CLArgs::parse_value<char>("\0\0"), CLArgs::ParseValueException<char>);
 }
