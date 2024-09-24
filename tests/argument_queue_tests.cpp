7ce15587f3e0e522014cd5b2a4670efb92d47511
Ruben Sørensen <73177332+rsore@users.noreply.github.com>
2024-09-25 00:18:08 +0200

    Implemented basic ArgumentQueue. Have size checking, front peeking and dequeuing.

--- tests/argument_queue_tests.cpp
+++ tests/argument_queue_tests.cpp
@@ -3,15 +3,29 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("ArgumentQueue tracks emptiness", "[ArgumentQueue]")
+TEST_CASE("ArgumentQueue can report the current size", "[ArgumentQueue]")
 {
-    constexpr std::array args{"foo"};
-    auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
+    {
+        constexpr std::array args{"foo"};
+        auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
 
-    CLArgs::ArgumentQueue queue(argc, argv);
-    CHECK(queue.empty() == false);
+        CLArgs::ArgumentQueue queue(argc, argv);
+        CHECK(queue.size() == 1);
+    }
+    {
+        constexpr std::array args{"foo", "bar"};
+        auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
 
-    // TODO: Dequeue element, check not empty
+        CLArgs::ArgumentQueue queue(argc, argv);
+        CHECK(queue.size() == 2);
+    }
+    {
+        constexpr std::array args{"foo", "bar", "baz"};
+        auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
+
+        CLArgs::ArgumentQueue queue(argc, argv);
+        CHECK(queue.size() == 3);
+    }
 }
 
 TEST_CASE("ArgumentQueue throws when invalid arguments are passed", "[ArgumentQueue]")
@@ -40,3 +54,76 @@ TEST_CASE("ArgumentQueue throws when invalid arguments are passed", "[ArgumentQu
         CHECK_THROWS(CLArgs::ArgumentQueue(argc, argv));
     }
 }
+
+TEST_CASE("Can get front element of ArgumentQueue", "[ArgumentQueue]")
+{
+    SECTION("Returns first element of queue when it exists")
+    {
+        {
+            constexpr std::array<const char *, 3> args{"foo", "bar", "baz"};
+            auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
+            CLArgs::ArgumentQueue queue(argc, argv);
+
+            CHECK(queue.front() == "foo");
+        }
+        {
+            constexpr std::array<const char *, 3> args{"bar", "baz", "foo"};
+            auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
+            CLArgs::ArgumentQueue queue(argc, argv);
+
+            CHECK(queue.front() == "bar");
+        }
+    }
+}
+
+TEST_CASE("Can dequeue front element, reducing the size of the queue", "[ArgumentQueue]")
+{
+    {
+        constexpr std::array args{"foo", "bar", "baz"};
+        auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
+
+        CLArgs::ArgumentQueue queue(argc, argv);
+        REQUIRE(queue.size() == 3);
+
+        REQUIRE(queue.dequeue() == "foo");
+        REQUIRE(queue.size() == 2);
+        REQUIRE(queue.front() == "bar");
+    }
+    {
+        constexpr std::array args{"qux", "baz", "bar", "foo"};
+        auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
+
+        CLArgs::ArgumentQueue queue(argc, argv);
+        REQUIRE(queue.size() == 4);
+
+        REQUIRE(queue.dequeue() == "qux");
+        REQUIRE(queue.size() == 3);
+        REQUIRE(queue.front() == "baz");
+    }
+}
+
+TEST_CASE("ArgumentQueue tracks emptiness", "[ArgumentQueue]")
+{
+    SECTION("Newly created queue is not empty")
+    {
+        constexpr std::array args{"foo"};
+        auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
+
+        CLArgs::ArgumentQueue queue(argc, argv);
+        CHECK(queue.empty() == false);
+    }
+
+    SECTION("Dequeuing empty queue throws exception")
+    {
+        constexpr std::array args{"foo"};
+        auto [argc, argv] = CLArgs::Testing::create_argc_argv_from_array(args);
+
+        CLArgs::ArgumentQueue queue(argc, argv);
+        REQUIRE(queue.size() == 1);
+        REQUIRE(!queue.empty());
+        REQUIRE_NOTHROW(std::ignore = queue.dequeue());
+        REQUIRE(queue.empty());
+        REQUIRE_THROWS(std::ignore = queue.dequeue());
+    }
+}
+
