90a28fe97a06d02d15f8168cafa3982789c8d394
Ruben Sørensen <73177332+rsore@users.noreply.github.com>
2024-10-20 16:19:59 +0200

    Adjusted parser tests to work with new API

--- tests/parser_tests.cpp
+++ tests/parser_tests.cpp
@@ -3,20 +3,23 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-using VerboseFlag = CLArgs::Flag<"--verbose,-v", "Enable verbose output">;
-using FileOption  = CLArgs::Option<"--file", "FILE", "Specify file to load", std::filesystem::path>;
+using VerboseFlag   = CLArgs::Flag<"--verbose,-v", "Enable verbose output">;
+using FlagList = CLArgs::CmdFlagList<VerboseFlag>;
+
+using ConfigOption = CLArgs::Option<"--config,--configuration,-c", "<filepath>", "Specify config file", std::filesystem::path>;
+using OptionList = CLArgs::CmdOptionList<ConfigOption>;
 
 TEST_CASE("Parse arguments", "[parse]")
 {
-    constexpr std::array args = {"program", "-v", "--file", "test.txt"};
+    constexpr std::array args = {"program", "-v", "--config", "test.txt"};
     auto [argc, argv]         = CLArgs::Testing::create_argc_argv_from_array(args);
 
-    CLArgs::Parser<VerboseFlag, FileOption> parser;
+    CLArgs::Parser<FlagList, OptionList> parser;
     REQUIRE_NOTHROW(parser.parse(argc, argv));
 
     REQUIRE(parser.has_flag<VerboseFlag>());
 
-    const auto file = parser.get_option<FileOption>();
+    const auto file = parser.get_option<ConfigOption>();
     REQUIRE(file.has_value());
     REQUIRE(file.value() == "test.txt");
 }
