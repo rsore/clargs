8274c9ec8e11535e14aa34ff010466167034a5fb
Ruben Sørensen <73177332+rsore@users.noreply.github.com>
2024-10-20 22:20:04 +0200

    User can pass program description to parser builder for help menu

--- examples/01_basic/main.cpp
+++ examples/01_basic/main.cpp
@@ -1,22 +1,26 @@
-#include <CLArgs/parser.hpp>
+#include <CLArgs/parser_builder.hpp>
 
 #include <filesystem>
+#include <iostream>
 
 using VerboseFlag   = CLArgs::Flag<"--verbose,-v", "Enable verbose output">;
 using QuietFlag     = CLArgs::Flag<"--quiet,-q", "Enable quiet output">;
 using RecursiveFlag = CLArgs::Flag<"--recursive,-r", "Enable recursion">;
 
-using FlagList = CLArgs::CmdFlagList<VerboseFlag, QuietFlag, RecursiveFlag>;
-
 using ConfigOption = CLArgs::Option<"--config,--configuration,-c", "<filepath>", "Specify config file", std::filesystem::path>;
 using NameOption   = CLArgs::Option<"--name,-n", "username", "Specify username", std::string>;
-
-using OptionList = CLArgs::CmdOptionList<ConfigOption, NameOption>;
 
 int
 main(int argc, char **argv)
 {
-    CLArgs::Parser<FlagList, OptionList> parser;
+    CLArgs::Parser parser = CLArgs::ParserBuilder{}
+                                .add_program_description<"Basic example program to showcase CLArgs library.">()
+                                .add_flag<VerboseFlag>()
+                                .add_flag<QuietFlag>()
+                                .add_flag<RecursiveFlag>()
+                                .add_option<ConfigOption>()
+                                .add_option<NameOption>()
+                                .build();
     try
     {
         parser.parse(argc, argv);
