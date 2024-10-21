5198d557104b01b47fb06100f1f974bc92b45ad8
Ruben Sørensen <73177332+rsore@users.noreply.github.com>
2024-10-22 01:38:59 +0200

    Added more common flags

--- include/CLArgs/common_flags.hpp
+++ include/CLArgs/common_flags.hpp
@@ -5,9 +5,18 @@
 
 namespace CLArgs::CommonFlags
 {
-    using Help    = Flag<"--help,-h", "Show help menu">;
-    using Verbose = Flag<"--verbose,-v", "Enable verbose output">;
-    using Quiet   = Flag<"--quiet,-q", "Enable quiet output">;
+    using Help         = Flag<"--help,-h", "Show help menu">;
+    using Verbose      = Flag<"--verbose,-v", "Enable verbose output">;
+    using Quiet        = Flag<"--quiet,-q", "Enable quiet output">;
+    using Version      = Flag<"--version", "Show program version">;
+    using Recursive    = Flag<"--recursive,-r", "Enable recursive mode">;
+    using All          = Flag<"--all,-a", "Include all">;
+    using Force        = Flag<"--force,-f", "Force the action">;
+    using Parallel     = Flag<"--parallel", "Enable parallel execution">;
+    using Experimental = Flag<"--experimental", "Enable experimental features">;
+    using Profile      = Flag<"--profile", "Profile program performance">;
+    using Debug        = Flag<"--debug", "Run in debug mode">;
+    using Overwrite    = Flag<"--overwrite", "Allow overwriting existing data">;
 } // namespace CLArgs::CommonFlags
 
 #endif
