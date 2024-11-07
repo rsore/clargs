097bf09c5b363b3cb059cb510899463a6cc5470f
Ruben Sørensen <73177332+rsore@users.noreply.github.com>
2024-11-07 23:36:57 +0100

    Added timestamps to log messages

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,13 +1,16 @@
+import datetime
+
 class Logger:
     def __init__(self, verbose=False):
         self.verbose = verbose
 
     def log(self, msg: str):
-        print(msg)
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] {msg}")
 
     def verbose_log(self, msg: str):
         if self.verbose:
-            print(msg)
+            self.log(msg)
 
 
 logger = Logger()
